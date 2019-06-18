0d44f664b42df1f8715ca48c523a904c57f02231
NITESCU Cristian <cristian.nitescu@renault.com>
2019-06-18 15:31:12 +0200

    Added remap feature

--- src/actuator.js
+++ src/actuator.js
@@ -10,6 +10,15 @@ class Actuator {
     setValue(usValue) {
         this.gpio.servoWrite(usValue);
     }
+    remap(value) {
+        const remap = this.config.remapValues;
+        if (!remap) return value;
+
+        if (value < -1) return remap[0];
+        if (value > 1) return remap[1];
+
+        return 1500 + value * (value < 0 ? 1500 - remap[0] : remap[1] - 1500 );
+    }
 }
 
 module.exports = { Actuator };
