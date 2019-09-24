660029a98de8ab91c7aeeead1a8f99ab58ecdfc0
NITESCU Cristian <cristian.nitescu@renault.com>
2019-09-24 18:42:56 +0200

    Added PID mode

--- src/actuator.js
+++ src/actuator.js
@@ -1,11 +1,22 @@
 const Gpio = require('pigpio').Gpio;
+const Controller = require('node-pid-controller');
 
 class Actuator {
     constructor(config) {
         this.config = config;
         this.gpio = new Gpio(this.config.pin, {mode: Gpio.OUTPUT}); 
         // initialize to middle
-        this.setValue(0);      
+        this.setValue(0); 
+        this.setSensorValue(0);
+        //
+        if (this.config.sensorMode && this.config.sensorMode !== 'none') {
+            this.pid = new Controller({
+                k_p: 0.25,
+                k_i: 0.01,
+                k_d: 0.01,
+                dt: 0.5
+            });
+        }  
     }
 
     setRemapMaxValue(value) {
@@ -14,7 +25,14 @@ class Actuator {
 
     setValue(value) {
         this.value = value;
-        this.gpio.servoWrite(this.remap(this.value));
+        if (this.pid) {
+            ctr.setTarget(value);
+        } else {
+            this.gpio.servoWrite(this.remap(this.value));            
+        }
+    }
+    setSensorValue(value) {
+        this.sensorValue = value;
     }
     getValue() {
         return this.value;
@@ -27,6 +45,13 @@ class Actuator {
         if (value > 1) return remap[1];
 
         return Math.round(1500 + value * (value < 0 ? 1500 - remap[0] : remap[1] - 1500 ));
+    }
+    updatePID() {
+        let newInput = this.pid.update(value);
+        if (this.config.sensorMode === 'invert') newInput = newInput * (-1);
+        if (math.abs(newInput) > this.config.remapPIDMax) newInput = this.config.remapPIDMax;
+
+        this.gpio.servoWrite(this.remap(newInput/this.config.remapPIDMax));
     }
 }
 
